e96690943a66ebbf5a3f6ef7d0ffe46918769c90
masaun <masanoriuno@gmail.com>
2020-08-16 08:30:55 +0900

    Fix indent

--- contracts/MaticEnergy.sol
+++ contracts/MaticEnergy.sol
@@ -35,11 +35,9 @@ contract MaticEnergy is ERC20, Whitelist, OwnableOriginal(msg.sender), McStorage
     uint128 public basePrice;
 
     /**
-     * @dev The constructor initializes the underlying currency token
-     * and the smart meter whitelist. The constructor also mints the
-     * requested amount of the underlying currency token to fund the
-     * network load. Also sets the base energy price, used for 
-     * calculating prices.
+     * @dev - The constructor initializes the underlying currency token and the smart meter whitelist. 
+     *      - The constructor also mints the requested amount of the underlying currency token to fund the network load. 
+     *      - Also sets the base energy price, used for calculating prices.
      */
     constructor (uint256 _initialSupply, uint128 _basePrice)
         public
@@ -59,11 +57,11 @@ contract MaticEnergy is ERC20, Whitelist, OwnableOriginal(msg.sender), McStorage
         returns(uint256)
     {
         return uint256(
-          max(
-            0,
-            int256(basePrice) *
-              (3 + safeSub(production[_time], consumption[_time]))
-          )
+            max(
+                0,
+                int256(basePrice) *
+                  (3 + safeSub(production[_time], consumption[_time]))
+            )
         );
     }
     
@@ -76,44 +74,42 @@ contract MaticEnergy is ERC20, Whitelist, OwnableOriginal(msg.sender), McStorage
         returns(uint256)
     {
         return uint256(
-          max(
-            0,
-            int256(basePrice) *
-              (3 + safeSub(consumption[_time], production[_time]))
-          )
+            max(
+                0,
+                int256(basePrice) *
+                  (3 + safeSub(consumption[_time], production[_time]))
+            )
         );
     }
     
     /**
-     * @dev Add one energy unit to the distribution network at the
-     * specified time and be paid the production price. Only
-     * whitelisted smart meters can call this function.
+     * @dev - Add one energy unit to the distribution network at the specified time and be paid the production price. 
+     *      - Only whitelisted smart meters can call this function.
      */
     function produce(uint256 _time)
         public
     {
         require(isMember(msg.sender), "Unknown meter.");
         this.transfer(
-          msg.sender,
-          getProductionPrice(_time)
+            msg.sender,
+            getProductionPrice(_time)
         );
         production[_time] = production[_time] + 1;
         emit EnergyProduced(msg.sender, _time);
     }
     
     /**
-     * @dev Take one energy unit from the distribution network at the
-     * specified time by paying the consumption price. Only
-     * whitelisted smart meters can call this function.
+     * @dev - Take one energy unit from the distribution network at the specified time by paying the consumption price. 
+     *      - Only whitelisted smart meters can call this function.
      */
     function consume(uint256 _time)
         public
     {
         require(isMember(msg.sender), "Unknown meter.");
         this.transferFrom(
-          msg.sender,
-          address(this),
-          getConsumptionPrice(_time)
+            msg.sender,
+            address(this),
+            getConsumptionPrice(_time)
         );
         consumption[_time] = consumption[_time] + 1;
         emit EnergyConsumed(msg.sender, _time);
@@ -131,8 +127,7 @@ contract MaticEnergy is ERC20, Whitelist, OwnableOriginal(msg.sender), McStorage
     }
     
     /**
-     * @dev Substracts b from a using types safely casting from
-     * uint128 to int256.
+     * @dev Substracts b from a using types safely casting from uint128 to int256.
      */
     function safeSub(uint128 a, uint128 b)
         internal
