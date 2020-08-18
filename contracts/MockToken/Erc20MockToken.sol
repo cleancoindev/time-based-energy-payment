055e61079a49ea2763308ecd913211492ff98a9c
masaun <masanoriuno@gmail.com>
2020-08-19 08:54:43 +0900

    Replace ERC20 by using @openzeppelin/contract v0.3.x (in Erc20MockToken.sol)

--- contracts/MockToken/Erc20MockToken.sol
+++ contracts/MockToken/Erc20MockToken.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";  /// From @openzeppelin/contract v0.3.x
 
 
 /***
  * @notice - This contract is the Test DAI Tokens (DAI) which is created by ERC20.
+ * @dev - Reference from @openzeppelin/contract v0.3.x https://docs.openzeppelin.com/contracts/3.x/erc20
  **/
-contract Erc20MockToken is ERC20, ERC20Detailed {
+contract Erc20MockToken is ERC20 {
 
-    constructor() ERC20Detailed("Erc20MockToken", "EMT", 18) public {
+    constructor() ERC20("Erc20MockToken", "EMT", 18) public {
         uint initialSupply = 1e8.mul(1e18);  /// Initial Supply amount is 100M
         address initialTokenHolder = msg.sender;
         _mint(initialTokenHolder, initialSupply);
