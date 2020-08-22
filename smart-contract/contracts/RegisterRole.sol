06b64b796df7ecb414903dcaa1ebfe19e3b90acd
masaun <masanoriuno@gmail.com>
2020-08-23 07:03:31 +0900

    Add Whitelist.sol into RegisterRole.sol

--- smart-contract/contracts/RegisterRole.sol
+++ smart-contract/contracts/RegisterRole.sol
@@ -1,7 +1,14 @@
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
+import "./@hq20/contracts/access/Whitelist.sol";
 
-contract RegisterRole {
-    /// In progress
+
+contract RegisterRole is {
+    constructor ()
+        public
+        Whitelist(msg.sender)  /// Add initial member
+    {
+
+    }
 }
